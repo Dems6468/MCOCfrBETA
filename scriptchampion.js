cd5be1fb8ec762d1d2892b0271823122b9e2aac1
Dems6468 <108577796+Dems6468@users.noreply.github.com>
2025-03-07 13:56:27 +0000

    Update scriptchampion.js

--- scriptchampion.js
+++ scriptchampion.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 const persoElement = document.createElement('div');
                 persoElement.classList.add('personnage-card');
                 persoElement.innerHTML = `
-                    <h3><a href="character.html?nom=${encodeURIComponent(perso.nom)}">${perso.nom}</a></h3>
+                    <h3><a href="characterchampion.html?nom=${encodeURIComponent(perso.nom)}">${perso.nom}</a></h3>
                     <p><strong>Classe :</strong> ${perso.classe}</p>
                 `;
                 personnageList.appendChild(persoElement);
